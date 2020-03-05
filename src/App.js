354154701a28a836d8a4cbd11a06e0e9fe585c6e
Moreshwar Dalvi <moreshwar.dalvi@bicsinfotech.com>
2020-03-05 23:55:16 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const emailRegex = RegExp();
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null,
+      contactNumber: null,
+      formErrors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+      }
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <h3>Create Account</h3>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                className=""
+                placeholder="First Name"
+                name="firstName"
+                onChange={this.handleChange}
+                noValidate
+              />
+            </div>
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                className=""
+                placeholder="Last Name"
+                name="lastName"
+                onChange={this.handleChange}
+                noValidate
+              />
+            </div>
+            <div className="contactNumber">
+              <label htmlFor="contactNumber">Contact Number</label>
+              <input
+                type="text"
+                className=""
+                placeholder="Contact Number"
+                name="contactNumber"
+                onChange={this.handleChange}
+                noValidate
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className=""
+                placeholder="Email"
+                name="email"
+                onChange={this.handleChange}
+                noValidate
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className=""
+                placeholder="Password"
+                name="password"
+                onChange={this.handleChange}
+                noValidate
+              />
+            </div>
+            <div className="createAccount">
+              <button type="submit">Create Account</button>
+              <small>Already have an account?</small>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
